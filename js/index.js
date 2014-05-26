3c0c5899f9ee05d84ce990dc21d75e7653306d3f
Fernando Rodriguez Sela <frsela@tid.es>
2014-05-26 16:25:33 +0200

    Refactor, clean and added beeps for some events

--- js/index.js
+++ js/index.js
@@ -1,83 +1,99 @@
-var CANVAS_OK = "#00CC66";
-var CANVAS_KO = "#FA5858";
-var CANVAS_WR = "#F4FA58";
+var CANVAS_OK = '#00CC66';
+var CANVAS_KO = '#FA5858';
+var CANVAS_WR = '#F4FA58';
 
-var CANVAS_STR_OK = "OK";
-var CANVAS_STR_KO = "CRITICAL";
-var CANVAS_STR_WR = "WARNING";
+var CANVAS_STR_OK = 'OK';
+var CANVAS_STR_KO = 'CRITICAL';
+var CANVAS_STR_WR = 'WARNING';
 
+function playBeep(code) {
+  var tonePlayer = new Audio();
+  switch (code) {
+    case 1:
+      tonePlayer.src = '/dtmf_tones/950Hz+1400Hz+1800Hz_200ms.ogg';
+      break;
+    case 2:
+      tonePlayer.src = '/dtmf_tones/480Hz+620Hz_200ms.ogg';
+      break;
+    default:
+      tonePlayer.src = '/dtmf_tones/400Hz_200ms.ogg';
+  }
+  tonePlayer.loop = false;
+  tonePlayer.play();
+}
 
 $(function() {
-  alert('Welcome to PushTester')
+//  alert('Welcome to PushTester');
 
   //Connection Status
-   var online = window.navigator.onLine;
-   console.log('Conectado: '+ online);
-   if (online){
-   	endpoint_register();
-   	fill_token();
-   	fill_canvas('pns_status',CANVAS_OK, CANVAS_STR_OK);
-   }
-   else{
-   	alert('Conecte el dispositivo a una red con datos');
-   }
+  window.onload = function() {
+    var online = window.navigator.onLine;
+    console.log('Conectado: ' + online);
+    if (online) {
+      endpoint_register();
+      fill_canvas('pns_status', CANVAS_OK, CANVAS_STR_OK);
+    } else {
+      alert('Conecte el dispositivo a una red con datos');
+    }
+  };
 });
 
-document.querySelector('#pushbtn').addEventListener ('click', function(){
-	alert('Sent new version to push notification server')
-	var version = new Date().getTime();
-	var token = localStorage.endpoint || null;
-	console.log('Sending notification to ' + URL + 'version: ' + version);
-	updateVersion(localStorage.endpoint, version);
-})
+document.querySelector('#pushbtn').addEventListener('click', function() {
+  alert('Sent new version to push notification server');
+  var version = new Date().getTime();
+  var token = localStorage.endpoint || null;
+  console.log('Sending notification to ' + URL + 'version: ' + version);
+  updateVersion(localStorage.endpoint, version);
+});
 
-function beep(severity, msj){
-	switch(severity){
-		case 'KO':
-			fill_canvas('pns_status', CANVAS_KO, CANVAS_STR_KO);
-			alert('Algo salió mal :( ' + '\r' + msj);
-			//beep :) 
-			break;
+function beep(severity, msj) {
+  switch (severity) {
+    case 'KO':
+      fill_canvas('pns_status', CANVAS_KO, CANVAS_STR_KO);
+      playBeep(2);
+      alert('Algo salió mal :( ' + '\r' + msj);
+      break;
 
-		case 'WR':
-			fill_canvas('pns_status', CANVAS_WR, CANVAS_STR_WR);
-			alert('Be carefully' + '\r' + msj);
-			break;
-	}
+    case 'WR':
+      fill_canvas('pns_status', CANVAS_WR, CANVAS_STR_WR);
+      playBeep();
+      alert('Be carefully' + '\r' + msj);
+      break;
+  }
 
 }
 
-//Poner cuadrado status con color y msg. Nueva func no hace falta msj, hará 
-function fill_canvas(id, color, string){
-	var c = document.getElementById(id);
-	var ctx = c.getContext("2d");
-	ctx.fillStyle = color;
-	ctx.fillRect(5,5,210,100);
-	ctx.fillStyle = "white";
-	ctx.font = "30px FiraSans";
-	ctx.fillText(string,77,45);
+//Poner cuadrado status con color y msg. Nueva func no hace falta msj, hará
+function fill_canvas(id, color, string) {
+  var c = document.getElementById(id);
+  var ctx = c.getContext('2d');
+  ctx.fillStyle = color;
+  ctx.fillRect(5, 5, 210, 100);
+  ctx.fillStyle = 'white';
+  ctx.font = '30px FiraSans';
+  ctx.fillText(string, 77, 45);
 }
 
-function fill_token(token){
-   	$('#AppToken').html('Mi AppToken: ' + token);
+function fill_token(token) {
+  console.log("token --------- " + token);
+  $('#AppToken').html('Mi AppToken: ' + token);
 }
 
 //Enviar nueva versión al AL de PNS.
-function updateVersion(URL,version) {
+function updateVersion(URL, version) {
   var oReq = new XMLHttpRequest();
   oReq.onload = function() {
-    $('lastversion_R').innerHTML = "Message=" + this.responseText;
-
+    $('lastversion_R').innerHTML = 'Message=' + this.responseText;
   };
-  console.log("URL: " + URL + " Version: "+ version);
+  console.log('URL: ' + URL + ' Version: ' + version);
   //oReq.open('put', $('endpointURL').innerHTML, true);
   //oReq.send('version='+$('channelVersion').value);
   oReq.open('put', URL, true);
   oReq.send('version=' + version);
   oReq.onerror = function() {
-    $('lastversion_R').innerHTML = "Error putting a new version";
-  }
-};
+    $('lastversion_R').innerHTML = 'Error putting a new version';
+  };
+}
 
 function updateVersion_noVersion() {
   var oReq = new XMLHttpRequest();
@@ -88,33 +104,36 @@ function updateVersion_noVersion() {
   oReq.send('version=');
 
   oReq.onerror = function() {
-    $('lastversion_R').innerHTML = "Error putting a new version";
-  }
-};
+    $('lastversion_R').innerHTML = 'Error putting a new version';
+  };
+}
 
 //Registro de AppToken
-function endpoint_register(){
-	//Now we call push.register() to request an endpoint
-	console.log('Registering PushTester in PNS');
-    var endpoint = localStorage.endpoint || null;
+function endpoint_register() {
+  //Now we call push.register() to request an endpoint
+  console.log('Registering PushTester in PNS');
+  var endpoint = localStorage.endpoint || null;
 
-    //Si se soportan notificaciones se activa el manejador de eventos de notificaciones
+  //Si se soportan notificaciones se activa
+  //el manejador de eventos de notificaciones
   if (navigator.push) {
+    playBeep();
+
     window.navigator.mozSetMessageHandler('push', function(evt) {
       console.log(evt.pushEndpoint);
 
 
-      if(endpoint == evt.pushEndpoint) {
+      if (endpoint == evt.pushEndpoint) {
         console.log('endpoint equals evt.pushEndpoint ' + endpoint);
-      var notification = navigator.mozNotification.createNotification('PushTester new version', 'version = ' + evt.version);
-      notification.show();
-      $('#lastversion_D').html(new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString());
+        var notification = navigator.mozNotification.createNotification(
+          'PushTester new version', 'version = ' + evt.version);
+        notification.show();
+        $('#lastversion_D').html(new Date().toLocaleDateString() + ' ' +
+          new Date().toLocaleTimeString());
       }
-
     });
 
     window.navigator.mozSetMessageHandler('push-register', function() {
-      
       navigator.push.unregister(endpoint);
 
       var req = navigator.push.register();
@@ -122,53 +141,54 @@ function endpoint_register(){
       req.onsuccess = function(e) {
         var endpoint = localStorage.endpoint = req.result;
         console.log('PUSH-REGISTER: nuevo endpoint --> ' + endpoint);
+        playBeep();
         fill_token(endpoint);
-      }
+      };
 
       req.onerror = function(e) {
-      	//aqui llamamos a beep
-       console.log('PUSH-REGISTER: error --> ' + JSON.stringify(e));
-       beep(KO, JSON.stringify(e));
-     }
-
+        //aqui llamamos a beep
+        console.log('PUSH-REGISTER: error --> ' + JSON.stringify(e));
+        beep('KO', JSON.stringify(e));
+      };
     });
-  }
-  //Si no se soportan notificaciones push
-  else{
-  	console.log('The browser doesn\'t support notifications push')
-  	//alert('The browser doesn\'t support notifications push')
-  	beep('WR','The browser doesn\'t support notifications push')
+  } else {
+    //Si no se soportan notificaciones push
+    console.log('The browser doesn\'t support notifications push');
+    //alert('The browser doesn\'t support notifications push')
+    beep('WR', 'The browser doesn\'t support notifications push');
   }
 
-  if(!endpoint){
+  if (!endpoint) {
     if (navigator.push) {
       var req = navigator.push.register();
 
       req.onsuccess = function(e) {
         endpoint = localStorage.endpoint = req.result;
         console.log('PUSH: nuevo endpoint --> ' + endpoint);
+        playBeep();
         fill_token(endpoint);
 
-      }
+      };
 
       req.onerror = function(e) {
-      	//aqui llamamos a beep
+        //aqui llamamos a beep
        console.log('PUSH: error endpoint--> ' + JSON.stringify(e));
-       beep(KO, JSON.stringify(e));
-      }
-   }
- } else {
-  endpoint = localStorage.endpoint;
-  console.log('PUSH: mi endpoint es: ' + endpoint);
-  fill_token(endpoint);
- }
+       beep('KO', JSON.stringify(e));
+      };
+    }
+  } else {
+    endpoint = localStorage.endpoint;
+    console.log('PUSH: mi endpoint es: ' + endpoint);
+    playBeep();
+    fill_token(endpoint);
+  }
 }
 
 function timeOut() {
-    window.alert('Hello!');
-    timeOut_init();   
+  window.alert('Hello!');
+  timeOut_init();
 }
 
 function timeOut_init() {
-    setTimeout('timeOut()', 5000);
+  setTimeout(timeOut, 5000);
 }
